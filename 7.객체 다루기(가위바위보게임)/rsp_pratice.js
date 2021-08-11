86eede462f9e995a40f1dff73e979ee647da2f33
Jay <63109802+bjw1622@users.noreply.github.com>
2021-08-11 15:22:22 +0900

    Add rsp  customizing and add css styling
    Add rsp  customizing and add css styling

--- 7.객체 다루기(가위바위보게임)/rsp_pratice.js
+++ 7.객체 다루기(가위바위보게임)/rsp_pratice.js
@@ -4,7 +4,7 @@ const $rock = document.querySelector("#rock");
 const $scissors = document.querySelector("#scissors");
 const $paper = document.querySelector("#paper");
 const IMG_URL = "./rsp.png";
-$computer.style.background = `url(${IMG_URL}) 0 0`;
+$computer.style.background = `url(${IMG_URL}) -220px 0`;
 $computer.style.backgroundSize = "auto 200px";
 
 const rspX = {
@@ -13,6 +13,11 @@ const rspX = {
   paper: "-440px", // 보
   };
 
+const rspComputer = {
+  scissors:0,
+  rock:1,
+  paper:2,
+}
   let computerChoice = "scissors";
   const change = () => {
     if (computerChoice === "rock") {
@@ -30,9 +35,35 @@ const rspX = {
   };
 let intervalId = setInterval(change, 50);
 let clickable = true;
-const clickButton = function(){
+let count = 0;
+const rspCount = {
+  가위:0,
+  바위:1,
+  보:2,
+};
+let score = 0;
+let answer = '';
+const clickButton = function(event){
+  const userTurn = rspCount[event.target.textContent];
+  const computerTurn = rspComputer[computerChoice];
+  const diff = userTurn - computerTurn;
   clearInterval(intervalId);
   clickable = false;
+  count += 1;
+  if([2,-1].includes(diff)){
+    score -= 1
+    answer= "패배하였습니다";
+  }else if([1,-2].includes(diff)){
+    score -= 1
+    answer= "패배하였습니다";
+    score += 1
+    answer = "축하합니다. 이겼습니다";
+  }else{
+    answer= "비겼습니다";
+
+  }
+  $score.innerHTML = `${count}판 play 했습니다.<br/> computer : ${computerChoice}, user : ${event.target.textContent}
+  <br/> 결과 : ${answer}, 점수 : ${score} `;
   setTimeout(()=>{
     intervalId = setInterval(change,50);
     clickable = true;
@@ -42,3 +73,4 @@ const clickButton = function(){
 $rock.addEventListener('click',clickButton);
 $scissors.addEventListener('click',clickButton);
 $paper.addEventListener('click',clickButton);
+
