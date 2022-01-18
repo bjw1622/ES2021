61393ff5a7f1c88d462ff4b035aaa586fa94e6dd
Jay <63109802+bjw1622@users.noreply.github.com>
2022-01-18 16:28:01 +0900

    function arguments 속성

--- 2.Syntax/함수.js
+++ 2.Syntax/함수.js
@@ -1,41 +1,61 @@
-function a() {
-  console.log(`안녕`);
-}
+// function a() {
+//   console.log(`안녕`);
+// }
 
-a();
+// a();
 
-function b() {
-  return 10;
-}
+// function b() {
+//   return 10;
+// }
 
-console.log(b());
+// console.log(b());
 
-function a(parameter) {
-  console.log(parameter);
-}
-a("argument");
+// function a(parameter) {
+//   console.log(parameter);
+// }
+// a("argument");
 
-function a(w, x, y, z) {
-  console.log(w, x, y, z);
-  console.log(arguments);
-}
+// function a(w, x, y, z) {
+//   console.log(w, x, y, z);
+//   console.log(arguments);
+// }
 
-a(1, 2, 3);
+// a(1, 2, 3);
 
-let d = (x, y, z) => {
-  return x * y * z;
-};
+// let d = (x, y, z) => {
+//   return x * y * z;
+// };
 
-console.log(d(1, 2, 3));
+// console.log(d(1, 2, 3));
 
-const arr = [`가`, `라`, `다`, `라`, `마`, `라`];
+// const arr = [`가`, `라`, `다`, `라`, `마`, `라`];
 
-while (true) {
-  let deleteArr = arr.indexOf("라");
+// while (true) {
+//   let deleteArr = arr.indexOf("라");
 
-  if (deleteArr === -1) {
-    break;
-  }
-  arr.splice(deleteArr, 1);
-}
-console.log(arr);
+//   if (deleteArr === -1) {
+//     break;
+//   }
+//   arr.splice(deleteArr, 1);
+// }
+// console.log(arr);
+
+// function a(w, x, y) {
+//   console.log(x, w, y);
+//   console.log(arguments);
+//   console.log(typeof arguments);
+//   console.log(arguments[0]);
+//   console.log(arguments[1]);
+//   console.log(arguments[2]);
+// }
+// a(1, 2, 3);
+
+// console.log(typeof []);
+// console.log(typeof {});
+
+// const b = function (a, b, c) {
+//   console.log(arguments);
+//   console.log(a, b, c);
+// };
+
+// b(1, 2, 3);
