c984391642c9d8d10d74123b1f559d9ddf035b8b
Jay <63109802+bjw1622@users.noreply.github.com>
2022-01-18 16:40:48 +0900

    객체 접근 방법

--- 2.Syntax/객체리터럴.js
+++ 2.Syntax/객체리터럴.js
@@ -1,21 +1,26 @@
 const jay = {
-  name : '백재원',
-  year : 1997,
-  month : 6,
-  day : 18,
-  gender : "M",
-}
+  name: "백재원",
+  year: 1997,
+  month: 6,
+  day: 18,
+  gender: "M",
+};
 
-console.log(jay.name);
-console.log(jay['name']);
-console.log(jay.dsda);
-// 없는 속성 추가하기.
-jay.weight = 180;
-console.log(jay.weight);
+// console.log(jay.name);
+// console.log(jay['name']);
+// console.log(jay.dsda);
+// // 없는 속성 추가하기.
+// jay.weight = 180;
+// console.log(jay.weight);
 
-jay.sda = 10;
-console.log(jay.sda);
+// jay.sda = 10;
+// console.log(jay.sda);
 
+// delete jay.sda;
+// console.log(jay);
 
-delete jay.sda;
-console.log(jay);
+console.log(jay.name); //대부분 온점을 활용하여 접근
+console.log(jay[`name`]);
+const target = "name";
+// console.log(jay["target"]);
+console.log(jay[target]);
