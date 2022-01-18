30fcd9e891a5b3de32ac839eec843fb87af7b0b7
Jay <63109802+bjw1622@users.noreply.github.com>
2022-01-18 17:24:16 +0900

    object reference

--- 2.Syntax/객체리터럴.js
+++ 2.Syntax/객체리터럴.js
@@ -25,14 +25,39 @@ const jay = {
 // // console.log(jay["target"]);
 // console.log(jay[target]);
 
-jay.name = "jay";
+// jay.name = "jay";
 
-delete jay.day; // 속성 삭제!
-console.log(jay);
+// delete jay.day; // 속성 삭제!
+// console.log(jay);
 
-const m = {
-  met: function (value) {
-    console.log(value);
-  },
-};
-m.met("test");
+// const m = {
+//   met: function (value) {
+//     console.log(value);
+//   },
+// };
+// m.met("test");
+
+// const a = { 1: "1" };
+// const b = { 1: "1" };
+
+// console.log(a === b);
+
+// 같은 객체인지 비교하기 위해선 기존 객체를 변수에 저장해야함.
+// const a = { name: "jay" };
+// const b = [1, 2, a];
+// if (a === b[2]) {
+//   console.log(true);
+// }
+
+// 참조
+// 객체를 저장한 변수를 다른 변수에 대입하면 두 변수 모두 같은 객체를 저장하는 셈.
+// a 와 b가 같은 객체를 참조하고 있다.
+// 객체에 해당하는 경우만 참조가 발생한다.
+const a = { name: `백재원`, age: 26 };
+const b = a;
+a.name = `jay`;
+console.log(b.name);
+const a = [1, 2, 3];
+const b = a;
+a[0] = 11;
+console.log(b);
