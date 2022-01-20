b7856ad2af68109b77bbdaf284ddf472280bc73f
Jay <63109802+bjw1622@users.noreply.github.com>
2022-01-20 14:04:49 +0900

    끝말잇기 완성

--- 3.DOM 객체 다루기/word-relay-with.js
+++ 3.DOM 객체 다루기/word-relay-with.js
@@ -3,9 +3,9 @@ const $ = (selector) => {
 };
 
 const number = parseInt(prompt("몇 명이 참가하나요?"), 10);
-let count = 0;
 let word; //제시어
 let newWord; // 새로 입력한 단어
+let count = 1;
 // input
 $("input").addEventListener("input", function (event) {
   newWord = event.target.value;
@@ -19,12 +19,32 @@ $("button").addEventListener("click", (event) => {
     word = newWord;
     $("#word").textContent = word;
     $("input").value = "";
+    $("input").focus();
+    // count
+    $("span").textContent = `${count}`;
+    count += 1;
+    if (count === number) {
+      count = 1;
+    }
   } else {
     if (newWord[0] === word[word.length - 1]) {
       word = newWord;
       $("#word").textContent = word;
       $("input").value = "";
+      $("input").focus();
+      // count
+      $("span").textContent = `${count}`;
+      count += 1;
+      if (count === number) {
+        count = 1;
+      }
     } else {
+      alert("틀렸습니다.");
+      $("input").value = "";
+      $("input").focus();
     }
   }
 });
+function changeWord{
+  
+}
