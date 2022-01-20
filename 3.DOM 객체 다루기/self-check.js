35b767698bf10cb4bb4f537732e260060d5052cf
Jay <63109802+bjw1622@users.noreply.github.com>
2022-01-20 17:39:00 +0900

    count 오류 수정

--- 3.DOM 객체 다루기/self-check.js
+++ 3.DOM 객체 다루기/self-check.js
@@ -3,6 +3,7 @@ const $ = (selector) => document.querySelector(selector);
 const number = prompt("몇 명 참가하나요?");
 let word;
 let newWord;
+let count = 0;
 // 참여 안 할 경우
 if (number === null) {
   alert("다음 기회에 참여해주세요");
@@ -20,6 +21,8 @@ $("button").addEventListener("click", (event) => {
       $(".word").textContent = `제시어 : ${word}`;
       $("input").value = "";
       $("input").focus();
+      $("span").textContent = `${count + 1}번째 참가자`;
+      count += 1;
     } else {
       alert("3글자로 입력해주세요");
       $("input").value = "";
@@ -28,15 +31,22 @@ $("button").addEventListener("click", (event) => {
   }
   // 제시어 존재
   else {
-    if (newWord.length === 3 && word[word.length - 1] === newWord[2]) {
+    if (newWord.length === 3 && word[2] === newWord[0]) {
       word = newWord;
       $(".word").textContent = `제시어 : ${word}`;
       $("input").value = "";
       $("input").focus();
+      $("span").textContent = `${count + 1}번째 참가자`;
+      if (count === number - 1) {
+        count = 0;
+      } else {
+        count += 1;
+      }
     } else {
-      alert("틀렸습니다");
+      alert("틀렸습니다. 다시 입력해주세요");
       $("input").value = "";
       $("input").focus();
+      $("span").textContent = `${count}번째 참가자`;
     }
   }
 });
