242cbac3ab94b558bb2d4ffed9bb9723dfc78e67
Jay <63109802+bjw1622@users.noreply.github.com>
2022-01-21 15:05:00 +0900

    result 기능 추가

--- 4.함수 사용하기/calculator.js
+++ 4.함수 사용하기/calculator.js
@@ -2,7 +2,7 @@ const $ = (selector) => document.querySelector(selector);
 let numOne = "";
 let operator = "";
 let numTwo = "";
-
+let result = "";
 // 리스너 함수의 자리에 맞게 return 타입으로 고차 함수의 형태로
 // 함수의 함수안에 실행하고자 하는 걸
 const onClickOperator = (ope) => () => {
@@ -40,5 +40,25 @@ $("#plus").addEventListener("click", onClickOperator("+"));
 $("#minus").addEventListener("click", onClickOperator("-"));
 $("#divide").addEventListener("click", onClickOperator("/"));
 $("#multiply").addEventListener("click", onClickOperator("*"));
-$("#calculate").addEventListener("click", () => {});
+$("#calculate").addEventListener("click", (event) => {
+  if (!numTwo) {
+    alert("숫자를 입력해주세요.");
+    return;
+  }
+  switch (operator) {
+    case "+":
+      result = Number(numOne) + Number(numTwo);
+      break;
+    case "-":
+      result = Number(numOne) - Number(numTwo);
+      break;
+    case "*":
+      result = Number(numOne) * Number(numTwo);
+      break;
+    case "/":
+      result = Number(numOne) / Number(numTwo);
+      break;
+  }
+  $("#result").value = result;
+});
 $("#clear").addEventListener("click", () => {});
