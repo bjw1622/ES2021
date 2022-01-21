26e99da07f79d12ade14d04aaf367db8fc89535b
Jay <63109802+bjw1622@users.noreply.github.com>
2022-01-21 14:16:46 +0900

    숫자 입력 및 연산자 입력

--- 4.함수 사용하기/calculator.js
+++ 4.함수 사용하기/calculator.js
@@ -5,28 +5,39 @@ let numTwo = "";
 
 // 리스너 함수의 자리에 맞게 return 타입으로 고차 함수의 형태로
 // 함수의 함수안에 실행하고자 하는 걸
-const onClickNumber = (number) => () => {
-  if (operator) {
-    numTwo += number;
+const onClickOperator = (ope) => () => {
+  if (!numOne) {
+    alert("숫자를 입력해주세요");
   } else {
-    numOne += number;
+    operator = ope;
   }
-  $("#result").value += number;
+  $("#operator").value = ope;
 };
 
-$("#num-0").addEventListener("click", onClickNumber(0));
-$("#num-1").addEventListener("click", () => {});
-$("#num-2").addEventListener("click", () => {});
-$("#num-3").addEventListener("click", () => {});
-$("#num-4").addEventListener("click", () => {});
-$("#num-5").addEventListener("click", () => {});
-$("#num-6").addEventListener("click", () => {});
-$("#num-7").addEventListener("click", () => {});
-$("#num-8").addEventListener("click", () => {});
-$("#num-9").addEventListener("click", () => {});
-$("#plus").addEventListener("click", () => {});
-$("#minus").addEventListener("click", () => {});
-$("#divide").addEventListener("click", () => {});
-$("#multiply").addEventListener("click", () => {});
+const onClickNumber = (event) => {
+  if (!operator === false && !numTwo === true) {
+    $("#result").value = "";
+  }
+  if (operator) {
+    numTwo += event.target.textContent;
+  } else {
+    numOne += event.target.textContent;
+  }
+  $("#result").value += event.target.textContent;
+};
+$("#num-0").addEventListener("click", onClickNumber);
+$("#num-1").addEventListener("click", onClickNumber);
+$("#num-2").addEventListener("click", onClickNumber);
+$("#num-3").addEventListener("click", onClickNumber);
+$("#num-4").addEventListener("click", onClickNumber);
+$("#num-5").addEventListener("click", onClickNumber);
+$("#num-6").addEventListener("click", onClickNumber);
+$("#num-7").addEventListener("click", onClickNumber);
+$("#num-8").addEventListener("click", onClickNumber);
+$("#num-9").addEventListener("click", onClickNumber);
+$("#plus").addEventListener("click", onClickOperator("+"));
+$("#minus").addEventListener("click", onClickOperator("-"));
+$("#divide").addEventListener("click", onClickOperator("/"));
+$("#multiply").addEventListener("click", onClickOperator("*"));
 $("#calculate").addEventListener("click", () => {});
 $("#clear").addEventListener("click", () => {});
