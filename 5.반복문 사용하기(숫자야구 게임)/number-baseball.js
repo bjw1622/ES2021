64086d51c8a8f9ab34ea194b065e89ecd08473d9
Jay <63109802+bjw1622@users.noreply.github.com>
2022-01-26 17:55:52 +0900

    add

--- 5.반복문 사용하기(숫자야구 게임)/number-baseball.js
+++ 5.반복문 사용하기(숫자야구 게임)/number-baseball.js
@@ -3,4 +3,8 @@ const$ = (selector) => document.querySelector(selector);
 // 숫자 랜덤으로 4자리 만들기
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const answer = [];
-for (let i = 0; i <= 3; i++) {}
+for (let i = 0; i <= 3; i++) {
+  const index = Math.floor(Math.random() * 9 + 1);
+
+  answer.push(numbers[index]);
+}
