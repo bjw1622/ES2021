caa171d184eafe9baf703e8a60f821436a6daaaf
Jay <63109802+bjw1622@users.noreply.github.com>
2022-01-28 10:21:28 +0900

    answer input

--- 5.반복문 사용하기(숫자야구 게임)/number-baseball.js
+++ 5.반복문 사용하기(숫자야구 게임)/number-baseball.js
@@ -4,7 +4,11 @@ const$ = (selector) => document.querySelector(selector);
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const answer = [];
 for (let i = 0; i <= 3; i++) {
-  const index = Math.floor(Math.random() * 9 + 1);
-
+  const index = Math.floor(Math.random() * 9);
+  if (index > numbers.length) {
+    answer.push(numbers[index - i]);
+    numbers.splice(index - i, index - i);
+  }
   answer.push(numbers[index]);
-}.
+  numbers.splice(index, index);
+}
