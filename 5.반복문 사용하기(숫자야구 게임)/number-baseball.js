ba3f78e0ac3fea856d1729d0eaea2b1fbd9ea3e2
Jay <bjw1622@gmail.com>
2022-02-07 17:58:25 +0900

    undefined 방지

--- 5.반복문 사용하기(숫자야구 게임)/number-baseball.js
+++ 5.반복문 사용하기(숫자야구 게임)/number-baseball.js
@@ -9,7 +9,9 @@ for (let n = 1; n < 10; n += 1) {
 
 const answer = [];
 for (let n = 0; n < 4; n += 1) {
-  const index = Math.floor(Math.random() * 9);
+  const index = Math.floor(Math.random() * (9 - n));
   answer.push(numbers[index]);
-  numbers.splice(index, i);
+  numbers.splice(index, 1);
 }
+
+console.log(answer);
