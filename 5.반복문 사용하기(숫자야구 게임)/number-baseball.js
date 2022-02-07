eaea51710f4a74b9934e1d5b7dca42b48c13655f
Jay <bjw1622@gmail.com>
2022-02-07 18:29:03 +0900

    from 태그 기본 동작 막기

--- 5.반복문 사용하기(숫자야구 게임)/number-baseball.js
+++ 5.반복문 사용하기(숫자야구 게임)/number-baseball.js
@@ -13,8 +13,9 @@ for (let n = 0; n < 4; n += 1) {
   answer.push(numbers[index]);
   numbers.splice(index, 1);
 }
+console.log(answer);
 
 // form안의 버튼을 클릭하면 submit 이벤트 발생
 $('#form').addEventListener('submit', event => {
-  console.log('hello');
+  event.preventDefault(); // 기본 동작 막기
 });
