e8a36fca23830537503a392e5b91c4e1287eecae
Jay <bjw1622@gmail.com>
2022-02-07 18:23:12 +0900

    form 이벤트

--- 5.반복문 사용하기(숫자야구 게임)/number-baseball.js
+++ 5.반복문 사용하기(숫자야구 게임)/number-baseball.js
@@ -1,5 +1,5 @@
 const $ = selector => {
-  return document.querySelector('selector');
+  return document.querySelector(selector);
 };
 
 const numbers = [];
@@ -9,9 +9,12 @@ for (let n = 1; n < 10; n += 1) {
 
 const answer = [];
 for (let n = 0; n < 4; n += 1) {
-  const index = Math.floor(Math.random() * (9 - n));
+  const index = Math.floor(Math.random() * numbers.length);
   answer.push(numbers[index]);
   numbers.splice(index, 1);
 }
 
-console.log(answer);
+// form안의 버튼을 클릭하면 submit 이벤트 발생
+$('#form').addEventListener('submit', event => {
+  console.log('hello');
+});
