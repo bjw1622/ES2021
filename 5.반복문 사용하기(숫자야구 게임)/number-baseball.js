b0bb486c812e012534e0c0c284ec72eee93af9c2
Jay <bjw1622@gmail.com>
2022-02-09 18:12:50 +0900

    number input error

--- 5.반복문 사용하기(숫자야구 게임)/number-baseball.js
+++ 5.반복문 사용하기(숫자야구 게임)/number-baseball.js
@@ -15,7 +15,17 @@ for (let n = 0; n < 4; n += 1) {
 }
 
 const tries = [];
-function checkInput(input) {}
+function checkInput(input) {
+  if (input.length !== 4) {
+    return alert('4자리를 입력해 주세요.');
+  }
+  if (new Set(input).size !== 4) {
+    return alert('중복되지 않게 입력해 주세요.');
+  }
+  if (tries.includes(input)) {
+    return alert('이미 시도한 값입니다.');
+  }
+}
 
 // form안의 버튼을 클릭하면 submit 이벤트 발생
 $('#form').addEventListener('submit', event => {
@@ -23,5 +33,8 @@ $('#form').addEventListener('submit', event => {
   const value = $('#input').value;
   $('#input').value = '';
   // 답안 검사 코드
-  checkInput(value);
+  if (checkInput(value)) {
+    tries.push(value); //입력 값 문제 없음
+  } else {
+  }
 });
