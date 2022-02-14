95275fc62acc6abbe72872ccb0093e484d490dce
Jay <bjw1622@gmail.com>
2022-02-14 09:48:52 +0900

    무작위로 공 섞기

--- 6.타이머 사용하기(로또 추첨기)/index.js
+++ 6.타이머 사용하기(로또 추첨기)/index.js
@@ -1,12 +1,15 @@
 // 45개의 숫자 생성
-const array = Array(45)
+const candidate = Array(45)
   .fill()
   .map((v, i) => i + 1);
 
 const shuffle = [];
-// 7개 숫자 중복없이 랜덤으로 뽑기
-for (let i = 0; i < 7; i++) {
-  const count = Math.floor(Math.random() * array.length);
-  shuffle.push(array[count]);
-  array.splice(count, 1);
+
+// 무작위로 공 뽑기
+// 피셔-예이츠 셔플
+while (candidate.length > 0) {
+  const random = Math.floor(Math.random() * candidate.length);
+  const spliceArray = candidate.splice(random, 1);
+  const value = spliceArray[0];
+  shuffle.push(value);
 }
