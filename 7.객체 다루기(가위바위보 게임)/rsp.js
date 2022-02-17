abf8692bf097e7ed503b53e5c4d6d4af7066e069
Jay <bjw1622@gmail.com>
2022-02-17 20:11:44 +0900

    setInterval 적용

--- 7.객체 다루기(가위바위보 게임)/rsp.js
+++ 7.객체 다루기(가위바위보 게임)/rsp.js
@@ -1,3 +1,37 @@
 const $ = selector => {
-  document.querySelector(selector);
+  return document.querySelector(selector);
 };
+
+const img = './rsp.png';
+$('#computer').style.background = `url(${img}) 0px 0`;
+$('#computer').style.backgroundSize = `auto 200px`;
+
+// 객체를 쓰는 이유는 그룹화
+const rsp = {
+  scissors: `0`,
+  rock: '-220px',
+  paper: `-448px`,
+};
+
+let coord = `0px`;
+
+let chageComputerHand = () => {
+  if (coord === rsp.scissors) {
+    //가위면 바위로
+    $('#computer').style.background = `url(${img}) ${rsp.rock} 0`;
+    $('#computer').style.backgroundSize = `auto 200px`;
+    coord = rsp.rock;
+  } else if (coord === rsp.rock) {
+    //바위면 보로
+    $('#computer').style.background = `url(${img}) ${rsp.paper} 0`;
+    $('#computer').style.backgroundSize = `auto 200px`;
+    coord = rsp.paper;
+  } else {
+    // 보면 가위로
+    $('#computer').style.background = `url(${img}) ${rsp.scissors} 0`;
+    $('#computer').style.backgroundSize = `auto 200px`;
+    coord = rsp.scissors;
+  }
+};
+
+setInterval(chageComputerHand, 50);
