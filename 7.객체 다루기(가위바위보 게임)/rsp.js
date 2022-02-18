c479d4b88cd154c335af5158c525fccbd292e555
Jay <bjw1622@gmail.com>
2022-02-18 21:13:03 +0900

    결과 코드 완성

--- 7.객체 다루기(가위바위보 게임)/rsp.js
+++ 7.객체 다루기(가위바위보 게임)/rsp.js
@@ -13,23 +13,29 @@ const rsp = {
   paper: `-448px`,
 };
 
-let coord = `scissors`;
-
-let changeComputerHand = () => {
-  if (coord === 'scissors') {
-    //가위면 바위로
-    coord = 'rock';
-  } else if (coord === 'rock') {
-    //바위면 보로
-    coord = 'paper';
-  } else if (coord === 'paper') {
-    // 보면 가위로
-    coord = 'scissors';
-  }
-  $('#computer').style.background = `url(${img}) ${rsp[coord]} 0`;
-  $('#computer').style.backgroundSize = `auto 200px`;
+const scoreTable = {
+  scissors: 1,
+  rock: 0,
+  paper: -1,
 };
 
+let computerChoice = `scissors`;
+
+let changeComputerHand = () => {
+  if (computerChoice === 'scissors') {
+    //가위면 바위로
+    computerChoice = 'rock';
+  } else if (computerChoice === 'rock') {
+    //바위면 보로
+    computerChoice = 'paper';
+  } else if (computerChoice === 'paper') {
+    // 보면 가위로
+    computerChoice = 'scissors';
+  }
+  $('#computer').style.background = `url(${img}) ${rsp[computerChoice]} 0`;
+  $('#computer').style.backgroundSize = `auto 200px`;
+};
+let result = 0;
 let intervalId = setInterval(changeComputerHand, 50);
 
 // 연달아 클릭하면 setTimeout이 여러번 실행
@@ -41,9 +47,29 @@ let intervalId = setInterval(changeComputerHand, 50);
 
 // 또 다른 방법으로 클릭 후 버튼 클릭이 안되도록 remove해주고 1초 후에 다시 addEventListner를 적용
 // 또 다른 방법으로 flag 변수를 하나 추가해서 if(flag)일때만 실행 가능하도록 코드 추가
-const clickButton = () => {
+const clickButton = event => {
   // 클릭 하자마자 실행
   clearInterval(intervalId);
+
+  // 점수 계산 및 화면 표시
+  let myChoice = 'test';
+  if (event.target.textContent === '가위') {
+    myChoice = 'scissors';
+  } else if (event.target.textContent === '바위') {
+    myChoice = 'rock';
+  } else if (event.target.textContent === '보') {
+    myChoice = 'paper';
+  }
+  const myScore = scoreTable[myChoice];
+  const computerScore = scoreTable[computerChoice];
+  const diff = myScore - computerScore;
+  if (diff === 2 || diff === -1) {
+    result += 1;
+  } else if (diff === 1 || diff === -2) {
+    result -= 1;
+  }
+  $('#score').textContent = `${result}`;
+
   setTimeout(() => {
     // 클릭 후 1초 후에 실행
     // 비동기 코드는 항상 조심해야 함.
