dbb775df567ce1eea0d04f17b84510d522afb134
Jay <bjw1622@gmail.com>
2022-02-24 16:56:57 +0900

    시간 저장 추가

--- 8.Date 사용하기(반응속도 테스트)/index.js
+++ 8.Date 사용하기(반응속도 테스트)/index.js
@@ -7,8 +7,8 @@ let endTime;
 let total = 0;
 let count = 0;
 $('#screen').addEventListener('click', event => {
+  //대기 화면
   if (event.target.classList.contains('waiting')) {
-    //대기 화면
     // $('#screen').classList.remove('waiting');
     // $('#screen').classList.add('ready');
     $('#screen').classList.replace('waiting', 'ready');
@@ -16,19 +16,24 @@ $('#screen').addEventListener('click', event => {
     setTimeout(() => {
       $('#screen').classList.replace('ready', 'now');
       $('#screen').textContent = '클릭하세요!!';
+      // 시작 시간 재기
       startTime = new Date().getTime();
     }, Math.floor(Math.random() * 1000) + 2000); // 2초 ~ 3초 사이에 변환
-  } else if (event.target.classList.contains('ready')) {
-    //준비 화면
+  }
+  //준비 화면
+  else if (event.target.classList.contains('ready')) {
     $('#screen').classList.replace('ready', 'waiting');
     $('#screen').textContent = '성급하게 클릭했습니다. ';
-  } else if (event.target.classList.contains('now')) {
-    //클릭 화면
+  }
+  //클릭 화면
+  else if (event.target.classList.contains('now')) {
+    // 끝 시간 재기
     endTime = new Date().getTime();
     let diff = endTime - startTime;
     total += diff;
     count += 1;
-    $('#result').textContent = `현재 ${diff}ms, 평균:${diff / count}ms`;
+    console.log(total, diff);
+    $('#result').textContent = `현재 ${diff}ms, 평균:${total / count}ms`;
     $('#screen').classList.replace('now', 'waiting');
     $('#screen').textContent = '클릭해서 시작하세요.';
   }
