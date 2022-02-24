f37abeb68c538bd4e3f247ffc96d58017dd5fc39
Jay <bjw1622@gmail.com>
2022-02-24 16:20:03 +0900

    remove, add, replace

--- 8.Date 사용하기(반응속도 테스트)/index.js
+++ 8.Date 사용하기(반응속도 테스트)/index.js
@@ -3,5 +3,15 @@ const $ = selector => {
 };
 
 $('#screen').addEventListener('click', event => {
-  console.log(event.target.classList.contains('waiting'));
+  if (event.target.classList.contains('waiting')) {
+    //대기 화면
+    // $('#screen').classList.remove('waiting');
+    // $('#screen').classList.add('ready');
+    $('#screen').classList.replace('waiting', 'ready');
+    $('#screen').textContent = '초록색이 되면 클릭하세요';
+  } else if (event.target.classList.contains('ready')) {
+    //준비 화면
+  } else if (event.target.classList.contains('now')) {
+    //클릭 화면
+  }
 });
