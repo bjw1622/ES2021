7d321a0849cbcf068c495ec019d3d50cf814083b
Jay <bjw1622@gmail.com>
2022-02-24 16:39:05 +0900

    성급한 클릭 방지

--- 8.Date 사용하기(반응속도 테스트)/index.js
+++ 8.Date 사용하기(반응속도 테스트)/index.js
@@ -4,6 +4,8 @@ const $ = selector => {
 
 let startTime;
 let endTime;
+let total = 0;
+let count = 0;
 $('#screen').addEventListener('click', event => {
   if (event.target.classList.contains('waiting')) {
     //대기 화면
@@ -11,7 +13,6 @@ $('#screen').addEventListener('click', event => {
     // $('#screen').classList.add('ready');
     $('#screen').classList.replace('waiting', 'ready');
     $('#screen').textContent = '초록색이 되면 클릭하세요';
-    $('#result').textContent = ``;
     setTimeout(() => {
       $('#screen').classList.replace('ready', 'now');
       $('#screen').textContent = '클릭하세요!!';
@@ -20,11 +21,14 @@ $('#screen').addEventListener('click', event => {
   } else if (event.target.classList.contains('ready')) {
     //준비 화면
     $('#screen').classList.replace('ready', 'waiting');
-    $('#screen').textContent = '성급하게 클릭했습니다.';
+    $('#screen').textContent = '성급하게 클릭했습니다. ';
   } else if (event.target.classList.contains('now')) {
     //클릭 화면
     endTime = new Date().getTime();
-    $('#result').textContent = `${endTime - startTime}ms`;
+    let diff = endTime - startTime;
+    total += diff;
+    count += 1;
+    $('#result').textContent = `현재 ${diff}ms, 평균:${diff / count}ms`;
     $('#screen').classList.replace('now', 'waiting');
     $('#screen').textContent = '클릭해서 시작하세요.';
   }
