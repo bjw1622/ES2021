c20ae4fbf0e8bfe68da2fb24c09dbef8f8b74fae
Jay <bjw1622@gmail.com>
2022-02-28 21:52:13 +0900

    start-screen 이벤트 리스너 추가

--- 10. 클래스 다루기(텍스트 RPG)/index.js
+++ 10. 클래스 다루기(텍스트 RPG)/index.js
@@ -1,1 +1,12 @@
 const $ = selector => document.querySelector(selector);
+
+// 자바스크립트 데이터와 화면이 같이 바뀌어야 한다!
+
+// 이름 입력 후 화면 전환
+$('#start-screen').addEventListener('submit', event => {
+  event.preventDefault();
+  const name = event.target['name-input'].value;
+  $('#start-screen').style.display = 'none';
+  $('#game-menu').style.display = 'block';
+  $('#hero-name').textContent = name;
+});
